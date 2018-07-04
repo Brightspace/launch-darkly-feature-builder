dd800dbde317c1a70cd2802cbecf325beed1a0d3
Jeff Ashton <Jeff.Ashton@D2L.com>
2018-07-03 21:21:16 -0400

    Ensuring all details of err get logged

--- src/log.js
+++ src/log.js
@@ -1,6 +1,19 @@
+const _ = require( 'lodash' );
 const bunyan = require( 'bunyan' );
 
 module.exports = bunyan.createLogger( {
 	name: 'launch-darkly-feature-builder',
-	level: process.env.LOG_LEVEL || 'info'
+	level: process.env.LOG_LEVEL || 'info',
+	serializers: {
+		err: function( err ) {
+
+			const serialized = bunyan.stdSerializers.err( err );
+			if( serialized === err ) {
+				return serialized;
+			}
+
+			const full = _.defaults( serialized, err );
+			return full;
+		}
+	}
 } );
